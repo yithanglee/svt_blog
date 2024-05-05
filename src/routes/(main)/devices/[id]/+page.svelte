08eaf7038974f67272c2dd298b3b95c9e18c62b1
Damien <yithanglee@gmail.com>
2024-05-05 17:09:45 +0800

    chore: add the customized box

--- src/routes/(main)/devices/[id]/+page.svelte
+++ src/routes/(main)/devices/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	/** @type {import('./$types').PageData} */
-	import { Badge } from 'flowbite-svelte';
+	import { Badge, Modal, Button } from 'flowbite-svelte';
+	import FormInput from '$lib/components/FormInput.svelte';
 	import SimpleTable from '$lib/components/SimpleTable.svelte';
 	import { goto } from '$app/navigation';
 	import { Socket } from 'phoenix';
@@ -12,13 +13,15 @@
 
 	export let data;
 	let device = data.device,
-		channel;
+		channel,
+		checkPage,
+		fdata = { delay: 0.1, value: 1 },
+		formModal = false;
 	console.info(device);
 	let isOnline = false;
 
 	const toggleOnlineStatus = (status) => {
 		isOnline = status;
-	
 	};
 
 	onMount(() => {
@@ -43,13 +46,20 @@
 		});
 
 		setInterval(() => {
-			console.log("set offline")
+			console.log('set offline');
 			isOnline = false;
 		}, 11000);
 	});
 	onDestroy(() => {
 		channel.leave();
 	});
+	function customCommand(data, zcheckPage) {
+		console.log('do mar');
+		console.log(data);
+		fdata = data;
+		checkPage = zcheckPage;
+		formModal = true;
+	}
 	function sendCommand(data, checkPage, confirmModal) {
 		console.log(data);
 		console.log('transfer approved!');
@@ -61,9 +71,9 @@
 					id: data.id,
 					name: device.name,
 					item_name: data.name,
-					value: data.value,
+					value: data.value, // reps
 					action: data.action,
-					delay: data.delay
+					delay: data.delay // delay
 				},
 				{
 					endpoint: url + '/svt_api/webhook',
@@ -74,14 +84,96 @@
 			);
 		});
 	}
+	function filteredInput(key) {
+		return null;
+	}
+	function tryPost() {
+		// postData(
+		// 	{ scope: 'mark_do', id: fdata.id, shipping_ref: fdata.shipping_ref, location_id: fdata.pick_up_point_id, status: 'sent' },
+		// 	{
+		// 		endpoint: url + '/svt_api/webhook',
+		// 		successCallback: () => {
+		// 			checkPage();
+		// 			formModal = false;
+		// 		}
+		// 	}
+		// );
+		postData(
+				{
+					scope: 'start_pwm',
+					id: fdata.id,
+					name: device.name,
+					item_name: data.name,
+					value: fdata.value, // reps
+					action: data.action,
+					delay: fdata.delay // delay
+				},
+				{
+					endpoint: url + '/svt_api/webhook',
+					successCallback: () => {
+						checkPage();
+						formModal = false;
+					}
+				}
+			);
+	}
 </script>
-<Badge style="position: absolute; right: 10vw;" class="text-large" large=true color="{isOnline ? 'green' : 'yellow'}">{isOnline ? 'Online' : '...'}</Badge>
+
+<Badge
+	style="position: absolute; right: 10vw;"
+	class="text-large"
+	large="true"
+	color={isOnline ? 'green' : 'yellow'}>{isOnline ? 'Online' : '...'}</Badge
+>
+
+<Modal bind:open={formModal} size="lg" autoclose={false} class="w-full" outsideclose>
+	<form class="flex flex-col space-y-6" id="currentForm" action="#">
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Confirmation</h3>
+		<p class="">Are you sure to send these custom command?</p>
+		<div
+			class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800 mt-4"
+			role="tabpanel"
+			aria-labelledby="id-tab"
+		>
+			<FormInput
+				module={'DeviceAction'}
+				key={{
+					label: 'delay',
+					expose: true
+				}}
+				data={fdata}
+				input={filteredInput({
+					label: 'delay',
+					expose: true
+				})}
+			/>
+			<FormInput
+				module={'DeviceAction'}
+				key={{
+					label: 'value',
+					expose: true
+				}}
+				data={fdata}
+				input={filteredInput({
+					label: 'value',
+					expose: true
+				})}
+			/>
+		</div>
+	</form>
+	<svelte:fragment slot="footer">
+		<Button on:click={() => tryPost()}>Submit</Button>
+	</svelte:fragment>
+</Modal>
 <SimpleTable
 	title={'Device - ' + device.name}
 	description={'Manual control device with following commands'}
 	data={{
 		apiData: { id: data.id },
-		buttons: [{ name: 'Control', onclickFn: sendCommand }],
+		buttons: [
+			{ name: 'Control', onclickFn: sendCommand },
+			{ name: 'Custom', onclickFn: customCommand }
+		],
 		scope: 'get_device_commands',
 		columns: [{ label: 'Name', data: 'name' }]
 	}}
